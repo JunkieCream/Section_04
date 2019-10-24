3588684645fa6861fc99ee6a93027a753c4a2b95
JunkieCream <cyriljoker@gmail.com>
2019-10-24 12:46:12 -0400

    BT17 First override

--- BattleTank 4.23/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank 4.23/Source/BattleTank/TankPlayerController.cpp
@@ -6,3 +6,20 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+	
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You don't posses a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("You posses %s"), *ControlledTank->GetName());
+	}
+}
