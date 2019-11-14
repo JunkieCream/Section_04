9e167ff190ab4944aaafc1e35f9d2c8f59a7d7cb
JunkieCream <cyriljoker@gmail.com>
2019-11-14 12:07:19 -0500

    BT21 Added HitLocation method

--- BattleTank 4.23/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank 4.23/Source/BattleTank/TankPlayerController.cpp
@@ -10,6 +10,37 @@ ATank* ATankPlayerController::GetControlledTank() const
 void ATankPlayerController::AimTowardCrosshair()
 {
 	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; // Out Parameter
+	GetSightHitLocation(HitLocation);
+	UE_LOG(LogTemp, Warning, TEXT("Hitlocation: %s"), *HitLocation.ToString());
+}
+
+bool ATankPlayerController::GetSightHitLocation(FVector& out_HitLocation)
+{
+	FHitResult OutHit;
+	FCollisionQueryParams CollisionParams;
+
+	FVector PlayerViewLocation;
+	FRotator PlayerViewRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewLocation,
+		OUT PlayerViewRotation
+	);
+
+	out_HitLocation = PlayerViewLocation + PlayerViewRotation.Vector()*1000.f;
+	DrawDebugLine(GetWorld(), PlayerViewLocation, out_HitLocation, FColor::Green, false, 1, 0, 1);
+
+	if (GetWorld()->LineTraceSingleByChannel(OutHit, PlayerViewLocation, out_HitLocation, ECC_Visibility, CollisionParams))
+	{
+		return true;
+	}
+	else 
+	{ 
+		return false; 
+	}
+
+	return false;
 }
 
 void ATankPlayerController::BeginPlay()
--- BattleTank 4.23/Source/BattleTank/TankPlayerController.h
+++ BattleTank 4.23/Source/BattleTank/TankPlayerController.h
@@ -5,6 +5,9 @@
 #include "Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
+#include "CollisionQueryParams.h"
+
 #include "TankPlayerController.generated.h"
 
 /**
@@ -26,4 +29,6 @@ public:
 
 	// Aim barrel of tank to crosshair
 	void AimTowardCrosshair();
+
+	bool GetSightHitLocation(FVector &out_HitLocation);
 };
